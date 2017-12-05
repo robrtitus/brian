0c1f8882ceb8aa111e24e4755c88724cf3ce1ce5
robert <robrtitus@users.noreply.github.com>
2017-12-05 14:27:11 -0800

    Update Program.cs
    fixed compiler errors

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,35 +24,33 @@ namespace ConsoleApp1
                 movies = new string[5];
 
                 //prompt the user for no of students' names to store
-                for ( i = 0; i  movies.Length; i++),
+                for ( i = 0; i < movies.Length; i++ )
                 {
-                    Console.Write( Enter movie name, ( i + 1 ), movies[i] );
+                    Console.Write( "Enter movie name: ", ( i + 1 ), movies[i] );
                     movies[i] = Console.ReadLine( );
 
-                    if ( movies[i] == exit )
-                        if string is equal to exit end loop
+                    //if string is equal to exit end loop
+                    if ( movies[i] == "exit" )
                     {
                         break;
                     }
                 }
 
-                displaying all elements of array
-
-
-            for ( i = 0; i  movies.Length; i++)
-            {
-                if ( string.IsNullOrEmpty( movies[i] )  movies[i] == exit)  if string is blank or equal to exit end loop
+                //displaying all elements of array
+                for ( i = 0; i < movies.Length; i++ )
                 {
-                    break;
+                    if ( string.IsNullOrEmpty( movies[i] ) || movies[i] == "exit" )  //if string is blank or equal to exit end loop
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine( string.Format( "Movie {0} is {1}" , ( i + 1 ), movies[i] ) );
                 }
 
-                Console.WriteLine( Movie {
-                    0} is  {
-                    1}, ( i + 1 ), movies[i]);
-                }
-
-
+                //you can 'start without debugging' (ctrl + F5)
+                //you won't need the readline
                 Console.ReadLine( );
             }
         }
     }
+}
